8c24f37947409aa2702aeb0b15f69b675fbc755f
Alexander Lukin <C:\Users\looki\AppData\Roaming\The Bat!>
2022-06-22 14:37:59 +0300

    save_to_file function was created

--- main.c
+++ main.c
@@ -16,13 +16,38 @@ Matrix create_matrix(int columns, int rows, double *content){
     for (int i = 0; i < rows; i++){
         for (int j = 0; j < columns; j++){
             *(m.data + i*columns + j) = *(content + i*columns + j);
-            printf("%lf ", *(m.data + i*columns + j));
         }
     }
     return m;
 }
 
+void print_matrix(Matrix matrix){
+    for(int i = 0; i < matrix.rows; i++){
+        printf("{ ");
+        for(int j = 0; j < matrix.columns; j++){
+            printf("%.3lf ", *(matrix.data + i*matrix.columns + j));
+        }
+        printf("}\n");
+    }
+}
 
+int save_to_file(char *filename, Matrix matrix){
+    FILE *file;
+    if ((file = fopen(filename, "w"))!= 0){
+        for(int i = 0; i < matrix.rows; i++){
+        fprintf(file,"{ ");
+        for(int j = 0; j < matrix.columns; j++){
+            fprintf(file, "%.3lf ", *(matrix.data + i*matrix.columns + j));
+        }
+        fprintf(file, "}\n");
+    }
+        fclose(file);
+        return 0;
+    } else {
+        fclose(file);
+        return 1;
+    }
+}
 
 int main(){
     double data[2][2] = {
@@ -32,10 +57,8 @@ int main(){
     double* p = &data[0][0];
     Matrix m = create_matrix(2,2, p);
 
-    printf("\n%lf\n", *(m.data + 0));
-    printf("%lf\n", *(m.data + 1));
-    printf("%lf\n", *(m.data + 2));
-    printf("%lf\n", *(m.data + 3));
+    print_matrix(m);
+    save_to_file("saves.txt", m);
 
     free(m.data);
 
